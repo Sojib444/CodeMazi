0898a7aae59be1650c33240825eea29e3480cee4
sojib <mdsojibhosen444@gmail.com>
2023-04-01 13:45:40 +0600

    Commit

---
+++ src/CodeMaze/Repository/Repository/RepositoryBase.cs
@@ -0,0 +1,44 @@
+﻿using Contract;
+using Microsoft.EntityFrameworkCore;
+using Repository.Context;
+using System.Linq.Expressions;
+
+namespace Persistance.Repository
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        public RepositoryBase(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+            _set = _applicationDbContext.Set<T>();
+        }
+
+        protected ApplicationDbContext _applicationDbContext { get; }
+        private DbSet<T> _set { get; }
+
+        public void Create(T entity)
+        {
+            _set.Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _set.Remove(entity);
+        }
+
+        public IQueryable<T> FindAll(bool trackChanges)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            _set.Update(entity);
+        }
+    }
+}
