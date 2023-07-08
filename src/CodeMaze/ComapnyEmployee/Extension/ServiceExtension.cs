6a6720156d911696a5eab6bbbcda84b28e20688f
sojib <mdsojibhosen444@gmail.com>
2023-07-08 07:08:44 +0600

    practice commit

---
+++ src/CodeMaze/ComapnyEmployee/Extension/ServiceExtension.cs
@@ -0,0 +1,12 @@
+﻿namespace ComapnyEmployee.Entension
+{
+    public static  class ServiceExtension
+    {
+        public static void ConfigureCQRS(this IServiceCollection service)
+        {
+            service.AddCors(action => action.AddPolicy("poliicy", builder =>
+                                   builder.AllowAnyOrigin().
+                                   AllowAnyHeader()));
+        }
+    }
+}
--- src/CodeMaze/ComapnyEmployee/Program.cs
+++ src/CodeMaze/ComapnyEmployee/Program.cs
@@ -1,12 +1,18 @@
+using ComapnyEmployee.Entension;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.ConfigureCQRS();
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
