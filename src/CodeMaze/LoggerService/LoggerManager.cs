ff52a8698888d73f98d59e3765ea3a8a7271c9ad
sojib <mdsojibhosen444@gmail.com>
2023-07-18 11:14:11 +0600

    added Logger service

---
+++ src/CodeMaze/LoggerService/LoggerManager.cs
@@ -0,0 +1,29 @@
+﻿using Contracts;
+using Serilog;
+
+namespace LoggerService
+{
+    public class LoggerManager : ILoggerManager
+    {
+
+        public void LogDebug(string message)
+        {
+            Log.Debug(message);
+        }
+
+        public void LogError(string message)
+        {
+            Log.Error(message);
+        }
+
+        public void LogInfo(string message)
+        {
+            Log.Information(message);
+        }
+
+        public void LogWarn(string message)
+        {
+            Log.Warning(message);
+        }
+    }
+}
