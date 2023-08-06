7a762527775f74fbfc1f0094682ae708054f72c8
sojib <mdsojibhosen444@gmail.com>
2023-08-06 20:15:48 +0600

    Working

---
+++ src/CodeMaze/Service.Contracts/IServiceManager.cs
@@ -0,0 +1,8 @@
+﻿namespace Service.Contracts
+{
+    public interface IServiceManager
+    {
+        ICompanyService companyService { get; set; }
+        IEmployeeService IEmployeeService { get; set; }
+    }
+}
